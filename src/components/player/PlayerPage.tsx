37ee7bd5352898c28dc222397da54ebf97518b5e
Sleroq <hireme@sleroq.link>
2026-01-21 04:58:15 +0500

    Update PlayerPage.tsx

--- src/components/player/PlayerPage.tsx
+++ src/components/player/PlayerPage.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import Player from './Player';
-import { useNavigate } from 'react-router-dom';
 import ModalTextInput from '../shared/ModalTextInput';
 
 const PlayerPage = () => {
