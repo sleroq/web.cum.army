06686f89be523d7c91846f4edc06cda38f9f3b48
Sleroq <hireme@sleroq.link>
2026-01-21 03:19:13 +0500

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	css: {
-		postcss: './postcss.config.js',
-	},
-	build: {
-		outDir: 'build',
-	},
-	server: {
-		open: true, // Opens browser on dev server start
-	},
-	envDir: '../',
-	// For backwards compatibility
-	envPrefix: ['REACT_', 'VITE_'],
+  plugins: [react(), tailwindcss()],
+  css: {
+    postcss: "./postcss.config.js",
+  },
+  build: {
+    outDir: "build",
+  },
+  server: {
+    open: true, // Opens browser on dev server start
+  },
+  // envDir: '../',
+  // For backwards compatibility
+  envPrefix: ["REACT_", "VITE_"],
 });
