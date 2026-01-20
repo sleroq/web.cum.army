538f6969cba57963e7fe2d90b7e924844588ad7b
Sleroq <hireme@sleroq.link>
2026-01-21 03:19:00 +0500

    Updated package derivation to build the vite app properly

--- package.nix
+++ package.nix
@@ -1,9 +1,32 @@
-{ stdenv, ... }:
-stdenv.mkDerivation {
-  name = "web-cum-army";
-  src = ./build;
+{ lib
+, buildNpmPackage
+}:
+
+buildNpmPackage {
+  pname = "web-cum-army";
+  version = "0.1.0";
+
+  # Build from source (Vite outputs to ./build via vite.config.mjs)
+  src = lib.cleanSourceWith {
+    src = ./.;
+    filter = path: type:
+      let
+        name = builtins.baseNameOf path;
+      in
+      # Avoid bundling local artifacts into the Nix source.
+      !(builtins.elem name [ "node_modules" "build" ]);
+  };
+
+  npmBuildScript = "build";
+
+  npmDepsHash = "sha256-suogIidJASSIKDB+Ym7O7v1v+80lq8SoG11HSzFUM8k=";
+
   installPhase = ''
-    mkdir -p $out
-    cp -R ./* $out/
+    runHook preInstall
+
+    mkdir -p "$out"
+    cp -R build/* "$out"/
+
+    runHook postInstall
   '';
 }
